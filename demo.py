a1b091c2b90bb2c38a2143e65925ee44ae517b40
Your Name <you@example.com>
2020-02-12 23:24:33 +0800

    init

---
+++ demo.py
@@ -0,0 +1,10 @@
+print(2 + 2)
+print(50 - 5 * 6)
+print(r'c:\python27\nou')
+
+x='abc'
+print('c:\\python27\\nou\\'+x)
+print(f'c:\\python27\\nou\\{x}')
+print('c:\\python27\\nou\\{}'.format(x))
+print("c:\\python27\\nou\\{}\\{}".format(x, 123))
+print("c:\\python27\\nou\\{x}\\{y}".format(x=x, y=123))
